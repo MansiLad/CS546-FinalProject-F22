fc9343a73b7bc453a4d59e3d4bb8ec5c1afec898
Dhruv Patel <68532749+patel78-spec@users.noreply.github.com>
2022-11-18 15:20:52 -0500

    added remove user

--- data/users.js
+++ data/users.js
@@ -34,3 +34,25 @@ const createUser = async (
 
 }
 
+const removeUser = async (userId) => {
+    const db = await dbConnection();
+    try {
+      userId = helper.checkId(userId);
+    } catch (e) {
+      throw e;
+    }
+  
+    const movieCollection = await movies();
+  
+    const user= await getMovieById(userId);
+  
+    const deletionInfo = await movieCollection.deleteOne({
+      _id: ObjectId(userId),
+    });
+  
+    if (deletionInfo.deletedCount === 0) {
+      throw `Could not delete  with id of ${userId}`;
+    }
+    return `${user.firstName} ${user.lastName} has been sucessfully deleted!`;
+  };
+
--- helpers.js
+++ helpers.js
@@ -1,2 +1,19 @@
 // sab apne functions idhr likho..
 // nai mai idhar nahi karugaa
+
+
+const checkId = (id) => {
+  if (!id)
+    throw { Status: "400", Message: "You must provide an id to search for" };
+  if (typeof id !== "string")
+    throw { Status: "400", Message: "Id must be a string" };
+  if (id.trim().length === 0)
+    throw {
+      Status: "400",
+      Message: "id cannot be an empty string or just spaces",
+    };
+  id = id.trim();
+  if (!ObjectId.isValid(id))
+    throw { Status: "400", Message: "invalid object ID" };
+  return id;
+};
