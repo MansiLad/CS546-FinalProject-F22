86de579b1d4ece778ba2c1ce59f7aef6e4face08
Dhruv Patel <68532749+patel78-spec@users.noreply.github.com>
2022-12-09 12:59:40 -0500

    addded some routes

--- routes/properties.js
+++ routes/properties.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const data = require("../data");
+const data = require("/data");
 const propertiesData = data.properties;
 const path = require("path");
 // const userData = data.users;
@@ -11,8 +11,31 @@ router.route("/").get(async (req, res) => {
 });
 
 router.route("/propUpload").post(async (req, res) => {
-  //code here for GET
+  //code here for post
   console.log(req.body.filename);
+});
+
+router.route("/properties").get(async (req, res) => {
+  //code here for GET
+  return res.render("Name of the template");
+});
+
+router.route("/filtered").post(async (req, res) => {
+  //code here for post
+  // function for filter
+  return res.render("Name of the template");
+});
+
+router.route("/removelisting").post(async (req, res) => {
+  //code here for post
+  id = req.params.id;
+  id = helper.chekId(id);
+  try {
+    propertiesData.removeListing(id);
+  } catch (error) {
+    
+  }
+  
 });
 
 module.exports = router;
