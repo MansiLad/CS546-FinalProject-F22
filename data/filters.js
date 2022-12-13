5b80ead5eea9bdbdbb7b3606a4d71fde978db136
sanika <schava12@stevens.edu>
2022-12-13 16:09:49 -0500

    made changes to filter.js

--- data/filters.js
+++ data/filters.js
@@ -70,6 +70,58 @@ module.exports = {
         return property;
       }
     }
-    
+    if(select_sortBy==="LowtoHigh"){
+      if(beds==="Any" && baths==="Any"){
+        const property = await propertyCollection.find({rent:{$gte:minimum,$lte:maximum}}).sort({rent: 1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else if(beds==="Any"){
+        baths = Number(baths)
+        const property = await propertyCollection.find({baths:baths,rent:{$gte:minimum,$lte:maximum}}).sort({rent: 1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else if(baths==="Any"){
+        beds=Number(beds)
+        const property = await propertyCollection.find({beds:beds,rent:{$gte:minimum,$lte:maximum}}).sort({rent: 1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else{
+        beds=Number(beds)
+        baths = Number(baths)
+        const property = await propertyCollection.find({beds:beds,baths:baths,rent:{$gte:minimum,$lte:maximum}}).sort({rent: 1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+
+    }
+    if(select_sortBy==="Recent"){
+      if(beds==="Any" && baths==="Any"){
+        const property = await propertyCollection.find({rent:{$gte:minimum,$lte:maximum}}).sort({datePosted: -1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else if(beds==="Any"){
+        baths = Number(baths)
+        const property = await propertyCollection.find({baths:baths,rent:{$gte:minimum,$lte:maximum}}).sort({datePosted: -1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else if(baths==="Any"){
+        beds=Number(beds)
+        const property = await propertyCollection.find({beds:beds,rent:{$gte:minimum,$lte:maximum}}).sort({datePosted: -1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+      else{
+        beds=Number(beds)
+        baths = Number(baths)
+        const property = await propertyCollection.find({beds:beds,baths:baths,rent:{$gte:minimum,$lte:maximum}}).sort({datePosted: -1}).toArray();
+        if (!property) throw 'property not found';
+        return property;
+      }
+    }
   },
 }
